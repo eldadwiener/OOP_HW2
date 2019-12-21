73c0b9c19c6c01f715c3a008742931ae965ab011
Sapir Malka <itssapir@gmail.com>
2019-12-21 19:54:41 +0200

    split graph nodes into 2 different maps for black and white nodes

--- BipartiteGraph.java
+++ BipartiteGraph.java
@@ -5,27 +5,37 @@ import java.util.Map;
 
 public class BipartiteGraph<T extends Comparable<T>> {
 	
-	private Map<T,Node<T>> nodes = new HashMap<>();
+	private Map<T,Node<T>> whiteNodes = new HashMap<>();
+	private Map<T,Node<T>> blackNodes = new HashMap<>();
 	
 	public boolean addNode (T nodeId, Node.NodeType type, Object obj) {
-		if ( nodes.containsKey(nodeId)) {
+		if ( whiteNodes.containsKey(nodeId) || blackNodes.containsKey(nodeId)) {
 			return false;
 		}
-		nodes.put(nodeId, new Node<T>(nodeId, type,obj));
+		if (type == Node.NodeType.BLACK) {
+			blackNodes.put(nodeId, new Node<T>(nodeId, type,obj));
+		}
+		else {
+			whiteNodes.put(nodeId, new Node<T>(nodeId, type,obj));
+		}
 		return true;
 	}
 	
 	public boolean addEdge (T edgeId, T srcNodeId, T dstNodeId) {
-		Node<T> srcNode = nodes.get(srcNodeId);
-		Node<T> dstNode = nodes.get(dstNodeId);
+		Node<T> srcNode;
+		Node<T> dstNode;
+		if (blackNodes.containsKey(srcNodeId)) {
+			srcNode = blackNodes.get(srcNodeId);
+			dstNode = whiteNodes.get(dstNodeId);
+		}
+		else {
+			srcNode = whiteNodes.get(srcNodeId);
+			dstNode = blackNodes.get(dstNodeId);
+		}
 		if (srcNode == null || dstNode == null) {
 			return false;
 		}
-		// both nodes exist, make sure they are of different types
-		if ( srcNode.getType() == dstNode.getType() ) {
-			return false;
-		}
-		// types are different, try adding the edge to both sides
+		// both nodes exist, types are different, try adding the edge to both sides
 		if ( !srcNode.insertChildEdge(edgeId, dstNode) ) {
 			return false;
 		}
@@ -40,7 +50,13 @@ public class BipartiteGraph<T extends Comparable<T>> {
 	}
 	
 	public boolean containsEdge (T srcNodeId, T dstNodeId) {
-		Node<T> srcNode = nodes.get(srcNodeId);
+		Node<T> srcNode;
+		if (blackNodes.containsKey(srcNodeId)) {
+			srcNode = blackNodes.get(srcNodeId);
+		}
+		else {
+			srcNode = whiteNodes.get(srcNodeId);
+		}
 		if (srcNode != null) {
 			return srcNode.isChild(dstNodeId);
 		}
@@ -48,25 +64,41 @@ public class BipartiteGraph<T extends Comparable<T>> {
 	}
 	
 	public boolean containsNode (T nodeId) {
-		return nodes.containsKey(nodeId);
+		return (blackNodes.containsKey(nodeId) || whiteNodes.containsKey(nodeId));
 	}
 	
 	public Node<T> getNode (T nodeId) {
-		return nodes.get(nodeId);
+		if (blackNodes.containsKey(nodeId)) {
+			return blackNodes.get(nodeId);
+		}
+		return whiteNodes.get(nodeId);
 	}
 	
 	public void removeNode (T nodeId) {
-		Node<T> removedNode = nodes.get(nodeId);
-		if (removedNode == null) {
-			return;
+		Node<T> removedNode;
+		if (blackNodes.containsKey(nodeId)) {
+			removedNode = blackNodes.get(nodeId);
+			removedNode.removeAllEdges();
+			blackNodes.remove(nodeId);
 		}
-		removedNode.removeAllEdges();
-		nodes.remove(nodeId);
+		else if(whiteNodes.containsKey(nodeId)){
+			removedNode = whiteNodes.get(nodeId);
+			removedNode.removeAllEdges();
+			whiteNodes.remove(nodeId);
+		}
 	}
 	
 	public void removeEdge (T srcNodeId, T dstNodeId) {
-		Node<T> srcNode = nodes.get(srcNodeId);
-		Node<T> dstNode = nodes.get(dstNodeId);
+		Node<T> srcNode;
+		Node<T> dstNode;
+		if (blackNodes.containsKey(srcNodeId)) {
+			srcNode = blackNodes.get(srcNodeId);
+			dstNode = whiteNodes.get(dstNodeId);
+		}
+		else {
+			srcNode = whiteNodes.get(srcNodeId);
+			dstNode = blackNodes.get(dstNodeId);
+		}
 		if (srcNode == null || dstNode == null) {
 			return;
 		}
